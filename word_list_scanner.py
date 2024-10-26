3c2dc56d014bc6bb0b39a321d0a3941d8ebe7c6a
Brian Barjenbruch <brian@kcpropertylaw.com>
2024-10-25 21:37:36 -0500

    Initial commit

--- word_list_scanner.py
+++ word_list_scanner.py
@@ -2,6 +2,7 @@ import re
 from dataclasses import dataclass
 from typing import List
 
+
 def scan_word_list(word_list: List[str], test_string: str) -> object:
 
     @dataclass
@@ -11,21 +12,21 @@ def scan_word_list(word_list: List[str], test_string: str) -> object:
         match_start: int
         match_end: int
 
-    test_string_list = re.split(r'\s+', test_string)
+    test_string_list = re.split(r"\s+", test_string)
     test_string_len = len(test_string_list)
     test_string_first_word = test_string_list[0]
 
     word_test_obj = WordTestResult(False, test_string_len, 0, 0)
 
-    for word_index,word in enumerate(word_list):
+    for word_index, word in enumerate(word_list):
         if word == test_string_first_word:
-            
-            sample_word_list = word_list[word_index:word_index+test_string_len]
+
+            sample_word_list = word_list[word_index : word_index + test_string_len]
 
             if sample_word_list == test_string_list:
                 word_test_obj.in_list = True
                 word_test_obj.match_start = word_index
                 word_test_obj.match_end = word_index + test_string_len - 1
                 break
-    
+
     return word_test_obj
