0d958254f5082eaab254aa46850b27164b7b032a
Brian Barjenbruch <brian@kcpropertylaw.com>
2024-10-26 14:44:55 -0500

    Changed variable name

--- word_list_scanner.py
+++ word_list_scanner.py
@@ -8,25 +8,25 @@ def scan_word_list(word_list: List[str], test_string: str) -> object:
     @dataclass
     class WordTestResult:
         in_list: bool
-        test_string_len: int
+        test_string_word_count: int
         match_start: int
         match_end: int
-        
+
     test_string_list = re.split(r"\s+", test_string)
-    test_string_len = len(test_string_list)
+    test_string_word_count = len(test_string_list)
     test_string_first_word = test_string_list[0]
 
-    word_test_obj = WordTestResult(False, test_string_len, 0, 0)
+    word_test_obj = WordTestResult(False, test_string_word_count, 0, 0)
 
     for word_index, word in enumerate(word_list):
         if word == test_string_first_word:
 
-            sample_word_list = word_list[word_index : word_index + test_string_len]
+            sample_word_list = word_list[word_index : word_index + test_string_word_count]
 
             if sample_word_list == test_string_list:
                 word_test_obj.in_list = True
                 word_test_obj.match_start = word_index
-                word_test_obj.match_end = word_index + test_string_len - 1
+                word_test_obj.match_end = word_index + test_string_word_count - 1
                 break
 
     return word_test_obj
